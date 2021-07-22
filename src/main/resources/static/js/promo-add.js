1f95ca7a4000d076e1a13fd1155beefaa538616d
Daniel Lopes <viana.daniel02@gmail.com>
2021-07-21 23:38:34 -0300

    (Front-end) Adicionando imagem de loading, enquanto carrega a requisição da imagem.

--- src/main/resources/static/js/promo-add.js
+++ src/main/resources/static/js/promo-add.js
@@ -10,7 +10,8 @@ $("#linkPromocao").on('change', function(){
                 $("#alert").removeClass("alert alert-danger").text("");
                 $("#titulo").val("");
                 $("#site").text("");
-                $("#linkImagem").attr("src", "/images/promo-dark.png");
+                $("#linkImagem").attr("src", "");
+                $("#loader-img").addClass("loader");
             },
             success: function(data){
                 console.log(data);
@@ -21,10 +22,15 @@ $("#linkPromocao").on('change', function(){
             statusCode: {
                 404: function(){
                     $("#alert").addClass("alert alert-danger").text("Nenhuma informação pode ser recuperado dessa URL.");
+                    $("#linkImagem").attr("src", "/images/promo-dark.png");
                 }
             },
             error: function(){
                 $("#alert").addClass("alert alert-danger").text("Algo deu errado.. Tente novamente mais tarde.");
+                $("#linkImagem").attr("src", "/images/promo-dark.png");
+            },
+            complete: function() {
+                $("#loader-img").removeClass("loader");
             }
         })
     }
