db6349f9bec9da0b2c837f40871f4c40e7ea702d
Daniel Lopes <viana.daniel02@gmail.com>
2021-07-24 01:19:37 -0300

    Adicionando funcionalidade p/ salvar uma nova promoção.

--- src/main/java/com/danieldev/springajax/controller/PromocaoController.java
+++ src/main/java/com/danieldev/springajax/controller/PromocaoController.java
@@ -1,14 +1,19 @@
 package com.danieldev.springajax.controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import com.danieldev.springajax.domain.Categoria;
+import com.danieldev.springajax.domain.Promocao;
 import com.danieldev.springajax.repository.CategoriaRepository;
+import com.danieldev.springajax.repository.PromocaoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,6 +22,17 @@ public class PromocaoController {
 
     @Autowired
     private CategoriaRepository categoriaRepository;
+
+    @Autowired
+    private PromocaoRepository promocaoRepository;
+
+    @PostMapping("/save")
+    public ResponseEntity<Promocao> salvarPromocao(Promocao promocao){
+        promocao.setDtCadastro(LocalDateTime.now());
+        promocaoRepository.save(promocao);
+
+        return ResponseEntity.ok().build();
+    }
 
     @ModelAttribute("categorias")
     public List<Categoria> getCategorias(){
--- src/main/resources/static/js/promo-add.js
+++ src/main/resources/static/js/promo-add.js
@@ -1,3 +1,33 @@
+$("#form-add-promo").submit(function(event){
+    event.preventDefault();
+
+    var promo = {};
+    promo.linkPromocao = $("#linkPromocao").val();
+    promo.descricao = $("#descricao").val();
+    promo.preco = $("#preco").val();
+    promo.titulo = $("#titulo").val();
+    promo.categoria = $("#categoria").val();
+    promo.linkImagem = $("#linkImagem").attr("src");
+    promo.site = $("#site").text();
+
+    console.log('promo > ', promo);
+
+    $.ajax({
+        method: 'POST',
+        url: "/promocao/save",
+        data: promo,
+        success: function(){
+            $("#alert").addClass("alert alert-success").text("Promoção salva com sucesso !");
+        },
+        error: function(xhr){
+            console.log("> error: ", xhr.responseText);
+            $("#alert").addClass("alert alert-danger").text("Erro ao salvar a promoção !");
+        }
+    })
+
+});
+
+
 $("#linkPromocao").on('change', function(){
     var url = $(this).val();
 
