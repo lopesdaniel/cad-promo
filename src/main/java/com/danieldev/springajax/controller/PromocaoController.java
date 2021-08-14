0ea4e6ebf8563460b9030d60fc4794ee4badaac8
Daniel Lopes <viana.daniel02@gmail.com>
2021-08-14 18:43:45 -0300

    Adicionando validação na classe PromocaoController.java

--- src/main/java/com/danieldev/springajax/controller/PromocaoController.java
+++ src/main/java/com/danieldev/springajax/controller/PromocaoController.java
@@ -1,7 +1,11 @@
 package com.danieldev.springajax.controller;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
 
 import com.danieldev.springajax.domain.Categoria;
 import com.danieldev.springajax.domain.Promocao;
@@ -11,6 +15,8 @@ import com.danieldev.springajax.repository.PromocaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,7 +33,17 @@ public class PromocaoController {
     private PromocaoRepository promocaoRepository;
 
     @PostMapping("/save")
-    public ResponseEntity<Promocao> salvarPromocao(Promocao promocao){
+    public ResponseEntity<?> salvarPromocao(@Valid Promocao promocao, BindingResult bindResult){
+
+        if(bindResult.hasErrors()){
+            Map<String, String> errors = new HashMap<>();
+            for(FieldError error : bindResult.getFieldErrors()){
+                errors.put(error.getField(), error.getDefaultMessage());
+            }
+
+            return ResponseEntity.unprocessableEntity().body(errors);
+        }
+
         promocao.setDtCadastro(LocalDateTime.now());
         promocaoRepository.save(promocao);
 
